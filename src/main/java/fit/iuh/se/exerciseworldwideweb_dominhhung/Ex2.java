61dd907ad27cee09f7153e4153fac7a811c5da54
MinhHung_T4TEK <dominhhung04032003@gmail.com>
2025-08-25 10:33:54 +0700

    done bài 2

---
+++ src/main/java/fit/iuh/se/exerciseworldwideweb_dominhhung/Ex2.java
@@ -0,0 +1,26 @@
+package fit.iuh.se.exerciseworldwideweb_dominhhung;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "indexServerlet", urlPatterns = {"/index"})
+public class Ex2 extends HttpServlet
+{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+
+        writer.println("<html><body>");
+        writer.println("<h1>Index Servlet 123</h1>");
+        writer.println("</body></html>");
+    }
+
+
+}
