2b51f76dc61814ab7c0832b0a2c29ebf8c9c32e7
MinhHung_T4TEK <dominhhung04032003@gmail.com>
2025-08-25 10:49:59 +0700

    done bài 3

--- src/main/java/fit/iuh/se/exerciseworldwideweb_dominhhung/Ex3.java
+++ src/main/java/fit/iuh/se/exerciseworldwideweb_dominhhung/Ex3.java
@@ -9,20 +9,17 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-//@WebServlet(name = "indexServerlet", urlPatterns = {"/index"})
-public class Ex2 extends HttpServlet
+//@WebServlet(name = "jsonServlet", urlPatterns = {"/json"})
+public class Ex3 extends HttpServlet
 {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html");
-        PrintWriter writer = resp.getWriter();
+        resp.setContentType("application/json");
 
-        writer.println("<html><body>");
-        writer.println("<h1>Index Servlet 123</h1>");
-        writer.println("<p><a href=\"http://localhost:8080/ExerciseWorldWideWeb_DoMinhHung_war_exploded/json\">Đi đến trang JSON</a></p>");
+        PrintWriter out = resp.getWriter();
 
-        writer.println("</body></html>");
+        out.println("<html><body>");
+        out.println("<h1>Servlet Ex3 application/jsom</h1>");
+        out.println("</body></html>");
     }
-
-
 }
